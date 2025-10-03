3e24e0977ae6ab09b8fd404c64249a3279fb0044
Bernapai <juanemeunier@gmail.com>
2025-10-02 22:49:11 -0300

    Add DTOS

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,17 @@
 import { Controller } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { MessagePattern } from '@nestjs/microservices';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { LoginUser, RegisterUser } from './dto';
 
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) { }
 
   @MessagePattern('register-user')
-  async registerUser() { }
+  async registerUser(@Payload() dto: RegisterUser) { }
 
   @MessagePattern('login-user')
-  async loginUser() { }
+  async loginUser(@Payload() dto: LoginUser) { }
 
   @MessagePattern('validate-user')
   async validateUser() { }
